63765a50c72e5a9ff5bd40a23f5622b182722179
Angela Vergara <angela_vergara@mckinsey.com>
2023-06-21 15:48:03 -0500

    fix: rollback legacy config

--- providers/index.js
+++ providers/index.js
@@ -5,12 +5,14 @@ const setupMongoDB = require('./mongo-client')
 
 module.exports = async () => {
   const adminSDK = setupFirebaseAdminSDKApp()
+  const adminAuth = adminSDK.auth()
   const dbInstance = adminSDK.firestore()
   const bucket = adminSDK.storage().bucket()
 
   const clientMongo = await setupMongoDB()
 
   return {
+    adminAuth,
     dbInstance,
     bucket,
     clientMongo,
