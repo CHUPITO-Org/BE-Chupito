16b0d92a6cd6e6b3003cc6d5551c318565e0169b
Angela Vergara <angela_vergara@mckinsey.com>
2023-06-21 15:45:54 -0500

    refactor: move env vars to the script

--- environment.js
+++ environment.js
@@ -17,26 +17,6 @@ function getEnvironmentVariables() {
   }
 }
 
-function getFirebaseConfig() {
-  return {
-    PRIVATE_KEY_ID: process.env.AUTH_PRIVATE_KEY_ID,
-    PRIVATE_KEY: JSON.parse(`"${process.env.AUTH_PRIVATE_KEY}"`),
-    CLIENT_EMAIL: process.env.AUTH_CLIENT_EMAIL,
-    CLIENT_ID: process.env.AUTH_CLIENT_ID,
-    CLIENT_X509_CERT_URL: process.env.AUTH_CLIENT_X509_CERT_URL,
-    PROJECT_ID: process.env.AUTH_PROJECT_ID,
-    API_KEY: process.env.AUTH_API_KEY,
-    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
-    STORAGE_BUCKET: process.env.AUTH_STORAGE_BUCKET,
-    MESSAGING_SENDER_ID: process.env.AUTH_MESSAGING_SENDER_ID,
-    APP_ID: process.env.AUTH_APP_ID,
-    MEASUREMENT_ID: process.env.AUTH_MEASUREMENT_ID,
-    TEST_EMAIL: process.env.TEST_USER_EMAIL,
-    TEST_PASSWORD: process.env.TEST_USER_PWD,
-  }
-}
-
 module.exports = {
   getEnvironmentVariables,
-  getFirebaseConfig,
 }
--- scripts/firebase-auth-test.js
+++ scripts/firebase-auth-test.js
@@ -1,17 +1,17 @@
-const environment = require('../environment')
+const dotenv = require('dotenv')
 const { getAuth, signInWithEmailAndPassword } = require('firebase/auth')
 const { initializeApp } = require('firebase/app')
 
-const firebaseSetup = environment.getFirebaseConfig()
+dotenv.config()
 
 const firebaseConfig = {
-  apiKey: firebaseSetup.API_KEY,
-  authDomain: firebaseSetup.AUTH_DOMAIN,
-  projectId: firebaseSetup.PROJECT_ID,
-  storageBucket: firebaseSetup.STORAGE_BUCKET,
-  messagingSenderId: firebaseSetup.MESSAGING_SENDER_ID,
-  appId: firebaseSetup.APP_ID,
-  measurementId: firebaseSetup.MEASUREMENT_ID,
+  apiKey: process.env.AUTH_API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.AUTH_PROJECT_ID,
+  storageBucket: process.env.AUTH_STORAGE_BUCKET,
+  messagingSenderId: process.env.AUTH_MESSAGING_SENDER_ID,
+  appId: process.env.AUTH_APP_ID,
+  measurementId: process.env.AUTH_MEASUREMENT_ID,
 }
 
 let app = null
@@ -28,8 +28,8 @@ const getFirebaseApp = () => {
 const runAuthentication = async () => {
   const auth = getAuth(getFirebaseApp())
 
-  const email = firebaseSetup.TEST_EMAIL
-  const password = firebaseSetup.TEST_PASSWORD
+  const email = process.env.TEST_USER_EMAIL
+  const password = process.env.TEST_USER_PWD
   signInWithEmailAndPassword(auth, email, password).then(userCredential => {
     userCredential.user.getIdToken().then(token => console.log('token: ', token))
   })
