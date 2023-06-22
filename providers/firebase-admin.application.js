446a652d354e6324db7e984906e03a51e7caed57
Angela Vergara <angela_vergara@mckinsey.com>
2023-06-22 17:34:49 -0500

    style: missing tabs

--- providers/firebase-admin.application.js
+++ providers/firebase-admin.application.js
@@ -7,19 +7,19 @@ const serviceAccount = require('./../services-config/app.json')
 
 dotenv.config()
 
-const account = { 
-...serviceAccount,
-project_id: process.env.AUTH_PROJECT_ID,
-private_key_id: process.env.AUTH_PRIVATE_KEY_ID,
-private_key: JSON.parse(`"${process.env.AUTH_PRIVATE_KEY}"`),
-client_email: process.env.AUTH_CLIENT_EMAIL,
-client_id: process.env.AUTH_CLIENT_ID,
-client_x509_cert_url: process.env.AUTH_CLIENT_CERT_URL,
+const account = {
+  ...serviceAccount,
+  project_id: process.env.AUTH_PROJECT_ID,
+  private_key_id: process.env.AUTH_PRIVATE_KEY_ID,
+  private_key: JSON.parse(`"${process.env.AUTH_PRIVATE_KEY}"`),
+  client_email: process.env.AUTH_CLIENT_EMAIL,
+  client_id: process.env.AUTH_CLIENT_ID,
+  client_x509_cert_url: process.env.AUTH_CLIENT_CERT_URL,
 }
 
 let app = null
 
-module.exports = () => { 
+module.exports = () => {
   if (!app) {
     app = admin.initializeApp({
       credential: admin.credential.cert(account),
