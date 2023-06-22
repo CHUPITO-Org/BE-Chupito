39512d463f4d2fd526d80cd5b1e59a6ea942a44b
Angela Vergara <angela_vergara@mckinsey.com>
2023-06-22 16:28:25 -0500

    feat: add todo for legacy code

--- providers/index.js
+++ providers/index.js
@@ -1,10 +1,11 @@
 'use strict'
-
+// TODO: Delete legacy connection setup after full refactor of usage.
 // const setupFirebaseAdminSDKApp = require('./firebase-admin.application')
 const setupFirebaseAuthSDKApp = require('./firebase-auth.application')
 const setupMongoDB = require('./mongo-client')
 
 module.exports = async () => {
+  // TODO: Delete legacy connection setup after full refactor of usage.
   // const adminSDK = setupFirebaseAdminSDKApp()
   const adminSDK = setupFirebaseAuthSDKApp()
   const adminAuth = adminSDK.auth()
