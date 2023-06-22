31945729079d819aafd3a4b54db33bddafc8092e
Angela Vergara <angela_vergara@mckinsey.com>
2023-06-22 16:09:27 -0500

    feat: implement token validation

--- providers/index.js
+++ providers/index.js
@@ -1,10 +1,12 @@
 'use strict'
 
-const setupFirebaseAdminSDKApp = require('./firebase-admin.application')
+// const setupFirebaseAdminSDKApp = require('./firebase-admin.application')
+const setupFirebaseAuthSDKApp = require('./firebase-auth.application')
 const setupMongoDB = require('./mongo-client')
 
 module.exports = async () => {
-  const adminSDK = setupFirebaseAdminSDKApp()
+  // const adminSDK = setupFirebaseAdminSDKApp()
+  const adminSDK = setupFirebaseAuthSDKApp()
   const adminAuth = adminSDK.auth()
   const dbInstance = adminSDK.firestore()
   const bucket = adminSDK.storage().bucket()
