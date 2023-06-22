4aa1fcae3aa09cf21b666eb5702e0bd4ff83fb07
Angela Vergara <angela_vergara@mckinsey.com>
2023-06-22 16:53:10 -0500

    fix: code review comments

--- providers/firebase-admin.application.js
+++ providers/firebase-admin.application.js
@@ -5,15 +5,17 @@ const dotenv = require('dotenv')
 
 const serviceAccount = require('./../services-config/app.json')
 
-const account = { ...serviceAccount }
-
 dotenv.config()
 
-account.private_key_id = process.env.PRIVATE_KEY_ID
-account.private_key = process.env.PRIVATE_KEY.replace(/\\n/g, '\n')
-account.client_email = process.env.CLIENT_EMAIL
-account.client_id = process.env.CLIENT_ID
-account.client_x509_cert_url = process.env.CLIENT_CERT_URL
+const account = { 
+...serviceAccount,
+project_id: process.env.AUTH_PROJECT_ID,
+private_key_id: process.env.AUTH_PRIVATE_KEY_ID,
+private_key: JSON.parse(`"${process.env.AUTH_PRIVATE_KEY}"`),
+client_email: process.env.AUTH_CLIENT_EMAIL,
+client_id: process.env.AUTH_CLIENT_ID,
+client_x509_cert_url: process.env.AUTH_CLIENT_CERT_URL,
+}
 
 let app = null
 
@@ -21,8 +23,7 @@ module.exports = () => {
   if (!app) {
     app = admin.initializeApp({
       credential: admin.credential.cert(account),
-      databaseURL: 'https://spidernode-app.firebaseio.com',
-      storageBucket: 'spider-node-app.appspot.com',
+      storageBucket: process.env.AUTH_STORAGE_BUCKET,
     })
   }
 
