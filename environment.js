0f8ff78bd845ba2bea58b63571e9d1639641eadb
Angela Vergara <angela_vergara@mckinsey.com>
2023-06-27 13:22:07 -0500

    refactor: scripts to merge functions

--- environment.js
+++ environment.js
@@ -3,8 +3,6 @@ const dotenv = require('dotenv')
 const DEFAULT_DATABASE = 'mongodb'
 
 // TODO: These should be removed because they are NOT environment variables
-const EVENTS_COLLECTION_NAME = 'events'
-const HEADQUARTERS_COLLECTION_NAME = 'headquarters'
 const APP_DB = 'mongodb'
 
 dotenv.config()
@@ -13,8 +11,6 @@ function getEnvironmentVariables() {
   return {
     MONGODB_URI: process.env.MONGODB_URI,
     DEFAULT_DB: DEFAULT_DATABASE,
-    EVENTS_COLLECTION: EVENTS_COLLECTION_NAME,
-    HEADQUARTERS_COLLECTION: HEADQUARTERS_COLLECTION_NAME,
     APP_DB,
   }
 }
